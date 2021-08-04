de27f1db04f567fc2c4451c22b9db34e55d7b02a
loliconshik3 <loliconshik3@gmail.com>
2021-08-04 10:21:01 +0300

    Now ledit creates config-dir in /home (linux)

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+mkdir ~/.ledit
+cp -n $SCRIPT_DIR/src/config.json ~/.ledit
+cp -n -R $SCRIPT_DIR/themes ~/.ledit
+cp -n -R $SCRIPT_DIR/syntax ~/.ledit
+
+clear
+
 python3 $SCRIPT_DIR/src/main.py
